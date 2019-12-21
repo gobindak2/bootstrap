c3a6f445b49cc4b0bdbeefcebfa848a5d316f692
gobindak2 <39517064+gobindak2@users.noreply.github.com>
2019-12-21 14:46:55 +0530

    Update bootstrap1.sh

--- bootstrap1.sh
+++ bootstrap1.sh
@@ -3,5 +3,5 @@ sudo apt-get update -y
 sudo apt-get install apache2 -y
 sudo su
 mv /var/www/html/index.html /var/www/html/index.html.original
-echo "<html></h2>Welcome to eduEnigma AWS Class</h2></html>" > /var/www/html/index.html
+echo "<html></h2>Welcome to AWS </h2></html>" > /var/www/html/index.html
 echo "<html></h2>Hello Students</h2></html>" > /var/www/html/index.html
